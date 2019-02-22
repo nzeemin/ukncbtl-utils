fe91006672d2f10b954b60405c7481562db64b5e
Yuriy Shestakov <yshestakov@gmail.com>
2019-02-23 01:12:44 +0200

    Usage #pragma pack()

--- rt11dsk/rt11date.h
+++ rt11dsk/rt11date.h
@@ -8,24 +8,26 @@ extern "C" {
 #include <time.h>
 #include <string.h>
 
+/*
 // https://stackoverflow.com/questions/1537964/visual-c-equivalent-of-gccs-attribute-packed
-#if defined(__GNUC__)
-    #define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
-#else
+#ifdef _MSC_VER
     #define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
+#else
+    #define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
 #endif
+*/
 
+#pragma pack(push,1)
 union rt11date {
     uint16_t    pac;
-PACK(
-    struct __attribute__((__packed__))  __i {
+    struct  __i {
         uint8_t year:5;
         uint8_t day:5;
         uint8_t mon:4;
         uint8_t age:2;
-    } i); 
-    
+    } i; 
 };
+#pragma pack(pop)
 
 uint16_t clock2rt11date(const time_t clock);
 void rt11date_str(uint16_t date, char* buf, size_t sz);
