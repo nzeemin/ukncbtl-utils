4eea18e09badf10e00282d772332dd8d15fd1ff9
Yuriy Shestakov <yshestakov@gmail.com>
2019-02-23 09:01:32 +0200

    Made MSVC compat fixes

--- rt11dsk/rt11date.cpp
+++ rt11dsk/rt11date.cpp
@@ -4,13 +4,12 @@
 uint16_t clock2rt11date(const time_t clock)
 {
     union rt11date  r = {0};
-    struct tm   v;
     uint16_t    year;
-    ::localtime_r(&clock, &v);
-    year = v.tm_year + 1900 - 1972;
+    struct tm   *v = ::localtime(&clock);
+    year = v->tm_year + 1900 - 1972;
     r.i.year = year % 32;
-    r.i.mon = v.tm_mon + 1;
-    r.i.day = v.tm_mday;
+    r.i.mon = v->tm_mon + 1;
+    r.i.day = v->tm_mday;
     r.i.age = year / 32;
     // printf("!date2rt11: a.m.d.y=%d.%d.%d.%d > 0x%04x\n", r.i.age, r.i.mon, r.i.day, r.i.year, r.pac);
     return r.pac;
@@ -18,6 +17,9 @@ uint16_t clock2rt11date(const time_t clock)
 
 
 /* */
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#endif
 
 void rt11date_str(uint16_t date, char* str, size_t sz)
 {
@@ -34,5 +36,5 @@ void rt11date_str(uint16_t date, char* str, size_t sz)
         strncpy(str, "  -BAD-  ", sz);
     else
         // sprintf(str, "[%d] %02d-%3s-%02d", age, day, months[month], year+1972);
-        snprintf(str, sz, "%02d-%3s-%02d", day, months[month], year >= 32? year+1972-2000: year+72);
+        ::snprintf(str, sz, "%02d-%3s-%02d", day, months[month], year >= 32? year+1972-2000: year+72);
 }
--- rt11dsk/rt11date.h
+++ rt11dsk/rt11date.h
@@ -8,15 +8,6 @@ extern "C" {
 #include <time.h>
 #include <string.h>
 
-/*
-// https://stackoverflow.com/questions/1537964/visual-c-equivalent-of-gccs-attribute-packed
-#ifdef _MSC_VER
-    #define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
-#else
-    #define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
-#endif
-*/
-
 #pragma pack(push,1)
 union rt11date {
     uint16_t    pac;
